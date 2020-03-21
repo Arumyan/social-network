e1eeec447f9ac0c53d12dc33d34b5d3aeabd9e45
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-03-21 09:14:22 +0300

    add ui login

--- src/App.js
+++ src/App.js
@@ -3,38 +3,45 @@ import './App.scss';
 
 import UsersList from './components/UsersList/UsersList';
 import Profile from './components/Profile/Profile';
+import Login from './components/Login/Login';
 
 import { Switch, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className='App'>
-      <header className="Header">
-        <div className="Logo">SocialNetwork</div>
-        <div className="User">
-          <span className="UserName">UserName</span>
-          <div className="Avatar">
-            <img src="https://specenergo.ru/sites/default/files/styles/mt_testimonial_image/public/2016-11/testimonial-4.jpg?itok=a7UblV6p" alt="Avatar"/>
+      <header className='Header'>
+        <div className='Logo'>SocialNetwork</div>
+        <div className='User'>
+          <span className='UserName'>UserName</span>
+          <div className='Avatar'>
+            <img
+              src='https://specenergo.ru/sites/default/files/styles/mt_testimonial_image/public/2016-11/testimonial-4.jpg?itok=a7UblV6p'
+              alt='Avatar'
+            />
           </div>
         </div>
       </header>
-      <div className="Content">
-        <nav className="Nav">
+      <div className='Content'>
+        <nav className='Nav'>
           <ul>
             <li>Users</li>
             <li>Messages</li>
             <li>Settings</li>
           </ul>
         </nav>
-        <main className="MainContent">
-        <Switch>
-          <Route path='/' exact>
-            <UsersList />
-          </Route>
-          <Route path='/profile/:userId'>
-            <Profile />
-          </Route>
-        </Switch>
+        <main className='MainContent'>
+          <Switch>
+            <Route path='/' exact>
+              <UsersList />
+            </Route>
+            <Route path='/profile/:userId'>
+              <Profile />
+            </Route>
+            <Route path='/Login' exact>
+              <Login />
+            </Route>
+          </Switch>
         </main>
       </div>
     </div>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import classes from './Login.module.scss'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className={classes.Login}>
+        <form className={classes.Form}>
+          <div className={classes.FormItem}>
+            <label htmlFor="">Login</label>
+            <input type="text" placeholder="Login"/>
+          </div>
+          <div className={classes.FormItem}>
+            <label htmlFor="">Password</label>
+            <input type="text" placeholder="Password"/>
+          </div>
+
+          <div className={classes.FormItemButton}>
+            <button>Login</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
