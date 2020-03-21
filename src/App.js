d40a1264b5584d49ce096a389b4f1a206121ab2c
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-03-21 10:30:31 +0300

    add login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
 import UsersList from './components/UsersList/UsersList';
@@ -7,45 +7,80 @@ import Login from './components/Login/Login';
 
 import { Switch, Route } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <header className='Header'>
-        <div className='Logo'>SocialNetwork</div>
-        <div className='User'>
-          <span className='UserName'>UserName</span>
-          <div className='Avatar'>
-            <img
-              src='https://specenergo.ru/sites/default/files/styles/mt_testimonial_image/public/2016-11/testimonial-4.jpg?itok=a7UblV6p'
-              alt='Avatar'
-            />
+import { connect } from 'react-redux';
+import axios from 'axios';
+
+class App extends Component {
+  componentDidMount() {
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/auth/me', {
+        withCredentials: true
+      })
+      .then(response => {
+
+        if(response.data.resultCode === 0) {
+          const {id, email, login} = response.data.data
+          this.props.setAuthUserData({id, email, login});
+        }
+      });
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div className='App'>
+        <header className='Header'>
+          <div className='Logo'>SocialNetwork</div>
+          <div className='User'>
+            <span className='UserName'>{this.props.isAuth ? this.props.login : 'Login'}</span>
+            <div className='Avatar'>
+              <img
+                src='https://specenergo.ru/sites/default/files/styles/mt_testimonial_image/public/2016-11/testimonial-4.jpg?itok=a7UblV6p'
+                alt='Avatar'
+              />
+            </div>
           </div>
+        </header>
+        <div className='Content'>
+          <nav className='Nav'>
+            <ul>
+              <li>Users</li>
+              <li>Messages</li>
+              <li>Settings</li>
+            </ul>
+          </nav>
+          <main className='MainContent'>
+            <Switch>
+              <Route path='/' exact>
+                <UsersList />
+              </Route>
+              <Route path='/profile/:userId'>
+                <Profile />
+              </Route>
+              <Route path='/Login' exact>
+                <Login />
+              </Route>
+            </Switch>
+          </main>
         </div>
-      </header>
-      <div className='Content'>
-        <nav className='Nav'>
-          <ul>
-            <li>Users</li>
-            <li>Messages</li>
-            <li>Settings</li>
-          </ul>
-        </nav>
-        <main className='MainContent'>
-          <Switch>
-            <Route path='/' exact>
-              <UsersList />
-            </Route>
-            <Route path='/profile/:userId'>
-              <Profile />
-            </Route>
-            <Route path='/Login' exact>
-              <Login />
-            </Route>
-          </Switch>
-        </main>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.authReducer.isAuth,
+    login: state.authReducer.login
+  };
 };
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    setAuthUserData: (data) => dispatch(
+      { type: 'SET_AUTH_USER_DATA', data }
+    )
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import usersReducer from './reducers/usersReducer';
+import authReducer from './reducers/authReducer';
 
 export default combineReducers({
+  authReducer,
   usersReducer
 });
