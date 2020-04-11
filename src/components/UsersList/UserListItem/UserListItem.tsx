cae86dcf856d0069f04b3ecd16c41235e6b5eaa2
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-04-11 18:50:07 +0300

    add types for userlistitem

---
+++ src/components/UsersList/UserListItem/UserListItem.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import classes from './UserListItem.module.scss';
+
+type PropsType = {
+  userId: number
+  photo: string
+  name: string
+  followed: boolean
+  follow: (userId:number) => void
+  unfollow: (userId:number) => void
+  followingInProgress: Array<number>
+  onClickUserItem: (userId:number) => void
+};
+
+const defaultImg =
+  'https://specenergo.ru/sites/default/files/styles/mt_testimonial_image/public/2016-11/testimonial-4.jpg?itok=a7UblV6p';
+
+const UserListItem: React.FC<PropsType> = ({
+  userId,
+  photo,
+  name,
+  followed,
+  follow,
+  unfollow,
+  followingInProgress,
+  onClickUserItem
+}) => {
+  return (
+    <div
+      onClick={() => onClickUserItem(userId)}
+      className={classes.UserListItem}
+    >
+      <div className={classes.UserListItemImg}>
+        <img src={photo ? photo : defaultImg} alt='img' />
+      </div>
+      <div className={classes.UserListItemName}>{name}</div>
+
+      {followed ? (
+        <button
+          disabled={followingInProgress.some(id => id === userId)}
+          onClick={e => {
+            e.stopPropagation();
+            unfollow(userId);
+          }}
+        >
+          Unfollow
+        </button>
+      ) : (
+        <button
+          disabled={followingInProgress.some(id => id === userId)}
+          onClick={e => {
+            e.stopPropagation();
+            follow(userId);
+          }}
+        >
+          Follow
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default UserListItem;
